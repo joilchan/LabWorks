06cb81a928bd31e08a2884268fecffff099159be
joil <lizunov2077@mail.ru>
2024-04-15 23:49:09 +0300

    add class, method and main

---
+++ LabWork5/Tasks/Client.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    internal class Client
+    {
+        private string Name { get; set; }
+        private string Address { get; set; }
+        private int Sum { get; set; }
+
+        public Client(string name, string address, int sum)
+        {
+            Name = name;
+            Address = address;
+            Sum = sum;
+        }
+
+        public Client() { }
+
+        public void Print()
+        {
+            Console.WriteLine($"Имя клиента: {Name}, адрес: {Address}, потраченная сумма: {Sum}");
+        }
+    }
+}
---
+++ LabWork5/Tasks/Program.cs
@@ -0,0 +1,7 @@
+﻿using Tasks;
+
+Client bily = new Client("Tom", "Павлоусова 2", 345); // вызываем конструктор с параметрами
+Client tom = new Client(); // вызываем конструктор по умолчанию
+
+bily.Print();
+tom.Print();
