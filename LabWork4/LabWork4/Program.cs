1a2d94c0eb72c119c5ef6d5e147105e587f03540
joil <lizunov2077@mail.ru>
2024-04-15 23:44:44 +0300

    add main

--- LabWork4/LabWork4/Program.cs
+++ LabWork4/LabWork4/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество строк массива: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов массива: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] numbers = new int[rows, columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        numbers[i, j] = -1;
+    }
+
+}
+
+//Random random = new Random();
+//for (int i = 0; i < rows; i++)
+//{
+//    for (int j = 0; i < columns; j++)
+//    {
+//        int randomRow = random.Next(rows);
+//        int randomColumns = random.Next(columns);
+//        numbers[randomRow, randomColumns] = -2;
+//    }
+//}
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+
+
+        Console.Write(numbers[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
